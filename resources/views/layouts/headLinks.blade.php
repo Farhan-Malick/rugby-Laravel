ea18d166162c940fce09f40fb24c69e8c849ead6
irfan77786 <hafizirfan8078@gmail.com>
2023-07-12 22:44:26 +0500

    navbar design

--- resources/views/layouts/headLinks.blade.php
+++ resources/views/layouts/headLinks.blade.php
@@ -13,5 +13,14 @@
 <link rel="stylesheet" href="{{asset('assets/fonts/flaticon/font/flaticon.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+<!-- Include Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css">
+<!-- Include animate.css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+<!-- Include your custom CSS file if any -->
 
+<!-- Include jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Include Bootstrap JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.min.js"></script>
 @vite('resources/js/app.js')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,52 +1,68 @@
 <header class="site-navbar py-4" role="banner">
 
     <div class="container">
-      <div class="d-flex align-items-center">
-        <div class="site-logo">
-            <h2><a href="{{URL('/')}}" class="nav-link">RUGBY</a></h2>
-            {{-- <img src="{{asset('assets/images/logo.png')}}" alt="Logo"> --}}
-        </div>
-        <div class="ml-auto">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-                <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                    <li class="active"><a href="{{URL('/')}}" class="nav-link">Home</a></li>
-                    <li><a href="{{URL('/matches')}}" class="nav-link">Matches</a></li>
-                    <li><a href="{{URL('/players')}}" class="nav-link">Players</a></li>
-                    {{-- <li><a href="blog.html" class="nav-link">Blog</a></li> --}}
-                    <li><a href="{{URL('/contact')}}" class="nav-link">Contact</a></li>
+        <div class="d-flex align-items-center">
+            <div class="site-logo">
+                <h2><a href="{{URL('/')}}" class="nav-link">RUGBY</a></h2>
+                {{-- <img src="{{asset('assets/images/logo.png')}}" alt="Logo"> --}}
+            </div>
+            <div class="ml-auto">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+                    <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                        <li class="active"><a href="{{URL('/')}}" class="nav-link"
+                                style=" transition: background-color 0.3s ease;">Home</a></li>
+                        <li><a href="{{URL('/matches')}}" class="nav-link">Matches</a></li>
+                        <li><a href="{{URL('/players')}}" class="nav-link">Players</a></li>
+                        <li><a href="{{URL('/contact')}}" class="nav-link">Contact</a></li>
 
-                    @if(Auth::check())
+                        @if(Auth::check())
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 {{ Auth::user()->first_name }}
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <a class="dropdown-item nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
-                                <a href="{{URL('/profile')}}" class="nav-link">Profile</a>
-                                <a href="{{URL('/myPicks')}}" class="nav-link">My Picks</a>
+                                <a href="{{URL('/profile')}}" class="dropdown-item nav-link">Profile</a>
+                                <a href="{{URL('/myPicks')}}" class="dropdown-item nav-link">My Picks</a>
                             </div>
                         </li>
-                    @else
+                        @else
                         <li><a href="{{URL('/login')}}" class="nav-link">Login</a></li>
                         <li><a href="{{URL('/register')}}" class="nav-link">Register</a></li>
+                        @endif
+                    </ul>
+                </nav>
 
-                    @endif
-                </ul>
-            </nav>
+                <a href="#"
+                    class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right text-white">
+                    <span class="icon-menu h3 text-white"></span>
+                </a>
+            </div>
+
+            <script>
+                $(document).ready(function() {
+                    // Add ease-out animation on hover
+                    $('.site-menu .nav-link').hover(
+                        function() {
+                            $(this).addClass('animate__animated animate__fadeIn');
+                        },
+                        function() {
+                            $(this).removeClass('animate__animated animate__fadeIn');
+                        }
+                    );
+                });
+            </script>
 
 
-          <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right text-white"><span
-              class="icon-menu h3 text-white"></span></a>
         </div>
-      </div>
     </div>
-  </header>
+</header>
